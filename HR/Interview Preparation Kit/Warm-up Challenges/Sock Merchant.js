15a2709a835beb7b6b3c8499600f105cfae4ed96
Oleksandr Martiuk <oleksandr.martiuk@gmail.com>
2020-03-04 16:39:47 +0200

    added more code for HR-IP-WarmUp

---
+++ HR/Interview Preparation Kit/Warm-up Challenges/Sock Merchant.js
@@ -0,0 +1,17 @@
+// Complete the sockMerchant function below.
+function sockMerchant(n, ar) {
+    let uniqueSocks = [];
+    let pairCount = 0;
+
+    for (let i = 0; i < ar.length; i++) {
+        let sock = ar[i];
+        if (uniqueSocks.includes(sock)) {
+            pairCount++;
+            uniqueSocks = uniqueSocks.filter(item => item !== sock);
+            continue;
+        }
+        uniqueSocks.push(sock);
+    }
+
+    return pairCount;
+}
