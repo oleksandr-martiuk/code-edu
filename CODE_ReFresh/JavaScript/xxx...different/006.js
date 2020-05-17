04719798213aeed8345b73ae131ffe02eac01609
Oleksandr Martiuk <oleksandr.martiuk@gmail.com>
2020-05-18 00:00:22 +0300

    updated WiKi

---
+++ CODE_ReFresh/JavaScript/xxx...different/006.js
@@ -0,0 +1,17 @@
+yo();
+
+function yo(params) {
+    // some code here....
+    function f_cb();
+}
+
+function f_cb(){
+    // some code here....
+    function s_cb(err, t_cb);
+}
+
+
+function t_cb(err, t_cb){
+    // some code here....
+    function s_cb(err, t_cb);
+}
---
+++ CODE_ReFresh/Node.js/001/childProces.js
@@ -0,0 +1,7 @@
+const childProcess = require('child_process');
+
+function execProcess(command) {
+    childProcess.exec(command, function(error, stdout, stderr) {
+        
+    })
+}
