368d96a9905f5541524ac509570979aa0f6018a0
Oleksandr Martiuk <oleksandr.martiuk@gmail.com>
2020-08-07 05:44:46 +0300

    added new task to the folder Challenges

---
+++ CODE_Challenge/CW/1_BECOME_IMMORTAL.js
@@ -0,0 +1,22 @@
+function elderAge(m,n,l,t) {
+
+  console.log('m=', m);
+  console.log('n=', n);
+  console.log('l=', l);
+  console.log('t=', t);
+  console.log('-----------------------------------------');
+
+  const num = m * n;
+  console.log(`num=${num}`);
+  const zero = (num/10) | 0 + 1;
+  console.log(`zero=${zero}`);
+
+  let sum = (m-1) * (m/2) * n;
+  console.log(`sum=${sum}`);
+  sum -= (m*n - zero) * l; // lost of seconds
+  console.log(`sum (l=${l}) =${sum}`);
+  if (sum >= t) sum -= t;  // lost big amount of time
+  console.log(`sum (t=${t}) =${sum}`);
+
+  return sum;
+}
