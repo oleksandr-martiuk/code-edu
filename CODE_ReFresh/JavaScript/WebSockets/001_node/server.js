5e4d63d9b5a3ed5f46abc98e0aea48c13436e701
Oleksandr Martiuk <oleksandr.martiuk@gmail.com>
2020-10-09 01:05:48 +0300

    minor updates for the WebSockets of CODE-ReFresh

--- CODE_ReFresh/JavaScript/WebSockets/001_node/server.js
+++ CODE_ReFresh/JavaScript/WebSockets/001_node/server.js
@@ -1,9 +1,9 @@
 const http = require('http');
 const ws = require('ws');
 
-const wss = new ws.Server({ noServer: true });
+const wss = new ws.Server({noServer: true});
 
-function accept (req, res) {
+function accept(req, res) {
   if (!req.headers.upgrade || req.headers.upgrade.toLowerCase() != 'websocket') {
     res.end();
     return;
@@ -18,11 +18,11 @@ function accept (req, res) {
 }
 
 function onConnect(ws) {
-  ws.on('message', function(message) {
-    let name = message.match(/([\p{Alpha}\p{M}\p{Nd}\p{Pc}\p{Join_C}]+)$/gu) || "Гость";
-    ws.send(`Hi from server, ${name}!`);
+  ws.on('message', function (message) {
+    let name = message.match(/([\p{Alpha}\p{M}\p{Nd}\p{Pc}\p{Join_C}]+)$/gu) || "Guest";
+    ws.send(`Hi from the server, ${name}!`);
 
-    setTimeout(() => ws.close(1000, 'Buy!'), 5000);
+    setTimeout(() => ws.close(1000, "Bay!"), 5000);
   });
 }
 
