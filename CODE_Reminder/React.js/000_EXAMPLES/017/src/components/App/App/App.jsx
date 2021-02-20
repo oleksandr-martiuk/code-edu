ec0a1852981b9647ffa22c0bdee0274f827d8607
Oleksandr Martiuk <oleksandr.martiuk@gmail.com>
2021-02-20 13:39:57 +0200

    React: minor updates

--- CODE_Reminder/React.js/000_EXAMPLES/017/src/components/App/App/App.jsx
+++ CODE_Reminder/React.js/000_EXAMPLES/017/src/components/App/App/App.jsx
@@ -4,12 +4,14 @@ import './app.scss';
 import {MyCounter} from '../Counter/Counter';
 import {MyTest} from "../MyTest/MyTest";
 import {Timer} from "../Timer/Timer";
+import {MyTest2} from "../MyTest2/MyTest2";
 
 export default function App({ title }) {
    return <div>
       <h1>{title}</h1><hr/>
       <MyCounter/><hr/>
       <MyTest some={'...test...'}/><hr/>
+      <MyTest2/>
       <Timer/>
    </div>
 }
---
+++ CODE_Reminder/React.js/000_EXAMPLES/017/src/components/App/MyTest2/MyTest2.jsx
@@ -0,0 +1,5 @@
+export const MyTest2 = () => {
+   return <>
+      Hello from Test2 component :)
+   </>
+}
